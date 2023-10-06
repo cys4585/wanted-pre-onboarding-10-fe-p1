d676702bef421e1b0ad2384993d4633dc3bc076a
cys4585 <soc4585@gmail.com>
2023-10-06 09:42:42 +0900

    feat: 로그인 기능 완성

--- src/pages/LoginWithMockAPI.tsx
+++ src/pages/LoginWithMockAPI.tsx
@@ -1,47 +1,119 @@
-import React from 'react'
+import React, { useState } from "react";
 
-type LoginSuccessMessage = 'SUCCESS'
-type LoginFailMessage = 'FAIL'
+type LoginSuccessMessage = "SUCCESS";
+type LoginFailMessage = "FAIL";
 
 interface LoginResponse {
-  message: LoginSuccessMessage | LoginFailMessage
-  token: string
+  message: LoginSuccessMessage | LoginFailMessage;
+  token: string;
 }
 
-const login = async (username: string, password: string): Promise<LoginResponse | null> => {
-  // TODO: 올바른 username, password를 입력하면 {message: 'SUCCESS', token: (원하는 문자열)} 를 반환하세요.
-  return null
+interface UserInfo {
+  name: string;
 }
 
-const getUserInfo = async (): Promise<{ username: string } | null> => {
-  // TODO: login 함수에서 받은 token을 이용해 사용자 정보를 받아오세요.
-  return null
+interface User {
+  username: string;
+  password: string;
+  userInfo: UserInfo;
 }
 
-const LoginWithMockAPI = () => {
-  const loginSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+const users: User[] = [
+  {
+    username: "blue",
+    password: "1234",
+    userInfo: { name: "blueStragglr" },
+  },
+  {
+    username: "white",
+    password: "1234",
+    userInfo: { name: "whiteDwarf" },
+  },
+  {
+    username: "red",
+    password: "1234",
+    userInfo: { name: "redGiant" },
+  },
+];
 
-    // TODO: form 에서 username과 password를 받아 login 함수를 호출하세요.
-    const formData = new FormData(event.currentTarget);
-    for (let [key, value] of formData.entries()) console.log(key, value);
+const _secret = "1234qwer!@#$";
+
+const login = async (
+  username: string,
+  password: string
+): Promise<LoginResponse | null> => {
+  const user: User | undefined = users.find(
+    (user: User) => user.username === username && user.password === password
+  );
+
+  return user
+    ? {
+        message: "SUCCESS",
+        token: JSON.stringify({ user: user.userInfo, secret: _secret }),
+      }
+    : null;
+};
+
+const getUserInfo = async (token: string): Promise<UserInfo | null> => {
+  const parsedToken = JSON.parse(token);
+  if (!parsedToken?.secret || parsedToken.secret !== _secret) {
+    return null;
   }
 
-  return (<div>
-    <h1>
-      Login with Mock API
-    </h1>
-    <form onSubmit={loginSubmitHandler}>
-      {/* TODO: 여기에 username과 password를 입력하는 input을 추가하세요. 제출을 위해 button도 추가하세요. */}
-    </form>
-    <div>
-      <h2>
-        User info
-      </h2>
-      {/* TODO: 유저 정보를 보여주도록 구현하세요. 필요에 따라 state나 다른 변수를 추가하세요. */}
-      {JSON.stringify({username: 'blueStragglr'})}
-    </div>
-  </div>)
-}
+  const loggedUser: User | undefined = users.find(
+    (user: User) => user.userInfo.name === parsedToken.user.name
+  );
+  return loggedUser ? loggedUser.userInfo : null;
+};
 
-export default LoginWithMockAPI
+const LoginWithMockAPI = () => {
+  const [userInfo, setUserInfo] = useState<UserInfo>({ name: "" });
+
+  const loginSubmitHandler = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
+
+    const loginRes = await login(username, password);
+    if (!loginRes) {
+      return;
+    }
+
+    const userInfo = await getUserInfo(loginRes.token);
+    if (!userInfo) {
+      return;
+    }
+
+    setUserInfo(userInfo);
+  };
+
+  return (
+    <div>
+      <h1>Login with Mock API</h1>
+      <form onSubmit={loginSubmitHandler}>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit" value="Submit">
+          submit
+        </button>
+      </form>
+      <div>
+        <h2>User info</h2>
+        {JSON.stringify(userInfo)}
+      </div>
+    </div>
+  );
+};
+
+export default LoginWithMockAPI;
